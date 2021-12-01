beabe60655805a0a2940d51d7b4ee957f42ce8fc
z4tz <z4tz87@gmail.com>
2021-12-01 11:21:34 +0100

    Day 1 solution

--- day1.py
+++ day1.py
@@ -1,8 +1,15 @@
 from inputreader import aocinput
 
 
+def depthincreases(depths, offset=1):
+    return sum([depths[i] < depths[i+offset] for i in range(len(depths)-offset)])
+
+
 def main(day):
     data = aocinput(day)
+    data = [int(line) for line in data]
+    print(depthincreases(data))
+    print(depthincreases(data, 3))
 
 
 if __name__ == '__main__':
